88f660ecf703d18db29c6e576e13e864d1dc4b11
Fernán González Domingo <fernangonzalez@gmail.com>
2018-01-17 00:43:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // write your code below!
+var name="Fernán"
+
 function happyHolidays() {
   return ("Happy holidays!")
 }
 
 function happyHolidaysTo(string) {
-  return
+  return `Happy holidays, ${name}!`
 }
